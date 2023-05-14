0b9ae568e853e95a5c7e9f219a18a708a034fead
Le Minh Tri <minhtri0795@gmail.com>
2023-05-14 15:46:54 +0700

    config Sanity client

--- client.ts
+++ client.ts
@@ -3,5 +3,6 @@ import sanityClient from '@sanity/client'
 export default sanityClient({
   projectId: process.env.SANITY_API_PROJECT_ID, // you can find this in sanity.json
   dataset: 'production', // or the name you chose in step 1
+  apiVersion: '2023-04-26',
   useCdn: true // `false` if you want to ensure fresh data
 })
